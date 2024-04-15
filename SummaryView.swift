5120cf15035411a9e46b19eb6efbc8a1139dd045
georgie naegele <gnaegele5254@stu.d214.org>
2024-04-15 13:17:20 -0500

    made a summary view with a text, depending on three varuables ( adjective, noun and verb)

---
+++ SummaryView.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Georgie Naegele on 4/15/24.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        
+        Text("Unfortunatley the" + adjective + noun + verb)
+        
+    }
+    
+}
