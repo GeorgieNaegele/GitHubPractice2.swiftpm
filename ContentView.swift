03b5855e4d471b0b59c8bc9cafa5ccceb4688884
Cait de  Wet <cdewet7499@stu.d214.org>
2024-04-15 13:05:36 -0500

    .....

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            NavigationLink("Click me!!!", destination: NounView())
+            NavigationLink("Click me!!", destination: NounView())
         }
     }
 }
